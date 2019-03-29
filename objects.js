a5e7649f5a2fc43832979e8e1bd9a2980e4e87d9
Leonardo Oliveira <leozartino@gmail.com>
2019-03-29 00:59:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -17,3 +17,20 @@ function updatePlaylist(p, artist, song){
 }
 
 console.log(updatePlaylist(playlist, 'Seu Jorge', 'Carolina'))
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+ 
+  return Object.assign({}, obj, { [key]: value })
+}
+  // it's important that we merge everything into
+  // a new object such as the empty {}. 
+    // Otherwise, the object obj will be modified. 
+    // Test what happens if this line was written as:
+    // return Object.assign(obj, { [key]: value })
+ 
+const recipe = { eggs: 3 }
+ 
+updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
+// returns `{ eggs: 3, chocolate: '1 cup' }`
+ 
+recipe // { eggs: 3 }

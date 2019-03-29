fa214e341442152f8bc00f6c2aae993aeefd3dd7
Leonardo Oliveira <leozartino@gmail.com>
2019-03-29 01:10:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@
 4. Create a function `removeFromPlaylist` that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
 */
 
-var playlist = {Slowdive:,My Bloody Valentine:, Phil Ochs:}
+var playlist = {Slowdive:'Alison',My Bloody Valentine:'Sometimes', Phil Ochs:}
 
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, {[artistName] : songTitle})

babb6d8d08b6f30fe052fbe25166570b32446521
Leonardo Oliveira <leozartino@gmail.com>
2019-03-29 00:59:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -30,7 +30,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
  
 const recipe = { eggs: 3 }
  
-updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
+console.log(updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup'))
 // returns `{ eggs: 3, chocolate: '1 cup' }`
  
 recipe // { eggs: 3 }
